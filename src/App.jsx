0ad56bc389cc876099c8928829d5004c3d426fe9
Vladyslav <51026693+vladyslav-dev@users.noreply.github.com>
2022-06-06 18:51:45 +0300

    Made 7 task

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import { useState } from 'react';
 import './styles/index.css';
 
 // Matrix 3 x 3 important
-const initMatrix = (defaultValue) => {
-  return Array.from(Array(3), () => new Array(3).fill(defaultValue));
+const initMatrix = (rows = 3, columns = 3, defaultValue = '') => {
+  const array = [];
+  for (let i = 0; i < rows; i++) {
+    array[i] = [];
+    for (let j = 0; j < columns; j++) {
+      array[i][j] = defaultValue;
+    }
+  }
+
+  return array;
 }
 
 // validate input function
 const validateInput = (value) => {
   return /^[0-9+-.\/]*$/.test(value);
 }
+
+const getTransposedMatrix = (matrix) => {
+  const transposedMatrix = initMatrix(matrix[0]?.length, matrix?.length);
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      transposedMatrix[j][i] = matrix[i][j];
+    }
+  }
+
+  return transposedMatrix;
+}
+
 
 const Matrix = ({
   disabled = false,
@@ -36,8 +57,10 @@ const Matrix = ({
 
 const App = () => {
 
-  const [matrixA, setMatrixA] = useState(() => initMatrix('2'));
-  const [matrixB, setMatrixB] = useState(() => initMatrix('3'));
+  const [matrixA, setMatrixA] = useState(() => initMatrix(2, 4, '11'));
+  const [matrixB, setMatrixB] = useState(() => initMatrix(3, 4, '22'));
+
+  const [transposedMatrix, setTransposedMatrix] = useState(null);
 
   const [result, setResult] = useState(null);
 
@@ -62,11 +85,32 @@ const App = () => {
   }
 
   const multiplyMatrices = () => {
+    const transposedMatrixB = getTransposedMatrix(matrixB);
+
+    setTransposedMatrix(transposedMatrixB);
 
     const A = matrixA.map(row => row.map(cell => parseInt(cell)));
-    const B = matrixB.map(row => row.map(cell => parseInt(cell)));
+    const B = transposedMatrixB.map(row => row.map(cell => parseInt(cell)));
 
-    const calcResult = [[], [], []];
+    const calcResult = [[], []];
+
+
+
+    // calcResult[0][0] = (A[0][0] * B[0][0]) + (A[0][1] * B[1][0]) + (A[0][2] * B[2][0]) + (A[0][3] * B[3][0]);
+    // calcResult[0][1] = (A[0][0] * B[0][1]) + (A[0][1] * B[1][1]) + (A[0][2] * B[2][1]) + (A[0][3] * B[3][1]);
+    // calcResult[0][2] = (A[0][0] * B[0][2]) + (A[0][1] * B[1][2]) + (A[0][2] * B[2][2]) + (A[0][3] * B[3][2]);
+
+    // calcResult[1][0] = (A[1][0] * B[0][0]) + (A[1][1] * B[1][0]) + (A[1][2] * B[2][0]) + (A[1][3] * B[3][0]);
+    // calcResult[1][1] = (A[1][0] * B[0][1]) + (A[1][1] * B[1][1]) + (A[1][2] * B[2][1]) + (A[1][3] * B[3][1]);
+    // calcResult[1][2] = (A[1][0] * B[0][2]) + (A[1][1] * B[1][2]) + (A[1][2] * B[2][2]) + (A[1][3] * B[3][2]);
+
+
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 3; j++) {
+        calcResult[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j] + A[i][2] * B[2][j] + A[i][3] * B[3][j];
+      }
+    }
+
 
     // result[0][0] = (A[0][0] * B[0][0]) + (A[0][1] * B[1][0]) + (A[0][2] * B[2][0]);
     // result[0][1] = (A[0][0] * B[0][1]) + (A[0][1] * B[1][1]) + (A[0][2] * B[2][1]);
@@ -80,11 +124,7 @@ const App = () => {
     // result[2][1] = (A[2][0] * B[0][1]) + (A[2][1] * B[1][1]) + (A[2][2] * B[2][1]);
     // result[2][2] = (A[2][0] * B[0][2]) + (A[2][1] * B[1][2]) + (A[2][2] * B[2][2]);
 
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        calcResult[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j] + A[i][2] * B[2][j];
-      }
-    }
+
 
     const checkIsValid = calcResult.every(row => row.every(cell => !isNaN(cell)));
 
@@ -95,15 +135,30 @@ const App = () => {
       alert('Invalid input');
     }
   }
-
+  console.log(transposedMatrix)
   return (
     <div className='wrapper'>
       <div className='matrix__container'>
-        <Matrix matrix={matrixA} changeHandler={changeHandlerMaxtrixA} />
-        <Matrix matrix={matrixB} changeHandler={changeHandlerMaxtrixB} />
+        <div className='matrix__item'>
+          <h2>Matrix A=</h2>
+          <Matrix matrix={matrixA} changeHandler={changeHandlerMaxtrixA} />
+        </div>
+        <div className='matrix__item'>
+          <h2>Matrix B=</h2>
+          <Matrix matrix={matrixB} changeHandler={changeHandlerMaxtrixB} />
+        </div>
       </div>
       <div className='matrix__container'>
-        {result !== null && <Matrix matrix={result} disabled />}
+        <div className='matrix__item'>
+        <h2>Transposed matrix B=</h2>
+        {transposedMatrix !== null && <Matrix matrix={transposedMatrix} disabled />}
+        </div>
+      </div>
+      <div className='matrix__container'>
+        <div className='matrix__item'>
+          <h2>Result=</h2>
+          {result !== null && <Matrix matrix={result} disabled />}
+        </div>
       </div>
       <button onClick={multiplyMatrices}>Calc</button>
     </div>
